effe45d73814b5c1867b46f48758afd63129b25f
Lorena C <lorena@eunknown.org>
2021-01-06 20:27:12 -0800

    fix css bug

--- 50_projects_in_50_days/Split Landing/script.js
+++ 50_projects_in_50_days/Split Landing/script.js
@@ -6,4 +6,4 @@ left.addEventListener('mouseenter', () => container.classList.add('hover-left'))
 left.addEventListener('mouseenter', () => container.classList.remove('hover-left'))
 
 right.addEventListener('mouseenter', () => container.classList.add('hover-right'))
-right.addEventListener('mouseenter', () => container.classList.add('hover-right'))
+right.addEventListener('mouseenter', () => container.classList.remove('hover-right'))
