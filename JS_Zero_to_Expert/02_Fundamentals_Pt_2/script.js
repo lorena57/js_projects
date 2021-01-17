db53424eefe897c86ccd8de2ecafc198748a5559
Lorena C <lorena@eunknown.org>
2021-01-16 17:48:26 -0800

    create a while loop

--- JS_Zero_to_Expert/02_Fundamentals_Pt_2/script.js
+++ JS_Zero_to_Expert/02_Fundamentals_Pt_2/script.js
@@ -98,11 +98,31 @@
 // console.log(lorenaArray[i])    
 // }
 
-for(let coding = 1; coding < 4; coding++){
-    console.log(`-----Start coding ${coding}`);
+// for(let coding = 1; coding < 4; coding++){
+//     console.log(`-----Start coding ${coding}`);
 
-    for(let language = 1; language < 6; language++){
-        console.log(`Learning my code repetition ${language}`)
-    }
+//     for(let language = 1; language < 6; language++){
+//         console.log(`Learning my code repetition ${language}`)
+//     }
 
+// }
+
+// for (let rep = 1; rep <= 10; rep++) {
+// console.log(`Lifting weights repetition ${rep}`
+//     );
+// }
+
+// let rep = 1;
+// while (rep <= 10 ) {
+//     console.log(`While: Lifting weights repetition ${rep}`
+//     )
+
+//     rep++;
+// }
+
+let dice = Math.trunc(Math.random() * 6) + 1;
+
+while(dice !== 6) {
+    console.log(`You rolled a ${dice}`)
+    dice = Math.trunc(Math.random() * 6) + 1;
 }
