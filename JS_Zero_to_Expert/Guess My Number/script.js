510a785d820a2b25d798a097e14086a886dda701
Lorena C <lorena@eunknown.org>
2021-01-19 20:33:45 -0800

    create guess function

--- JS_Zero_to_Expert/Guess My Number/script.js
+++ JS_Zero_to_Expert/Guess My Number/script.js
@@ -12,10 +12,21 @@
 
 // document.querySelector('.guess').value;
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+
 document.querySelector('.check').addEventListener('click', function () {
+  //const guess gets the value that was submitted
   const guess = Number(document.querySelector('.guess').value);
 
+  //!guess will turn false to true and true to false
   if (!guess) {
     document.querySelector('.message').textContent = '🚫 No number';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct Number!';
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = '📈 Too high!';
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = '📉 Too low!';
   }
 });
