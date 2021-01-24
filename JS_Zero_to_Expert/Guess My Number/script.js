c878049aea33944978d20d901da1bd8e02883181
Lorena C <lorena@eunknown.org>
2021-01-23 22:32:59 -0800

    update number function

--- JS_Zero_to_Expert/Guess My Number/script.js
+++ JS_Zero_to_Expert/Guess My Number/script.js
@@ -15,6 +15,8 @@
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 document.querySelector('.number').textContent = secretNumber;
 
+let score = 20;
+
 document.querySelector('.check').addEventListener('click', function () {
   //const guess gets the value that was submitted
   const guess = Number(document.querySelector('.guess').value);
@@ -25,8 +27,22 @@ document.querySelector('.check').addEventListener('click', function () {
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number!';
   } else if (guess > secretNumber) {
-    document.querySelector('.message').textContent = '📈 Too high!';
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
   } else if (guess < secretNumber) {
-    document.querySelector('.message').textContent = '📉 Too low!';
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Too Low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
