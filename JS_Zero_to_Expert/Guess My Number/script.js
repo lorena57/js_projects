2a7cb65e5d6a85315e20d8283015cfc9c50162f7
Lorena C <lorena@eunknown.org>
2021-01-24 21:12:36 -0800

    remove unnecessary code

--- JS_Zero_to_Expert/Guess My Number/script.js
+++ JS_Zero_to_Expert/Guess My Number/script.js
@@ -1,17 +1,5 @@
 'use strict';
 
-// document.querySelector('.message').textContent;
-
-// document.querySelector('.message').textContent = '🎉 Correct Number!';
-
-// document.querySelector('.number').textContent = 13;
-
-// document.querySelector('.score').textContent = 10;
-
-// document.querySelector('.guess').value = 23;
-
-// document.querySelector('.guess').value;
-
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 let score = 20;
