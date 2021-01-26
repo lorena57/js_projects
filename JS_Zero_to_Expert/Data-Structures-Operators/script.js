10adcfafdcafc20a512ef3c2d13571c231af3d46
Lorena C <lorena@eunknown.org>
2021-01-25 21:15:29 -0800

    pull and destructure array

--- JS_Zero_to_Expert/Data-Structures-Operators/script.js
+++ JS_Zero_to_Expert/Data-Structures-Operators/script.js
@@ -7,18 +7,44 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
 };
+
+const [starter, mainMeal] = restaurant.order(2, 0);
+console.log(starter, mainMeal);
+
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+// const [x, y, z] is not an array, It's just a destructuring assignment
+//  = arr is the array from which I want to get the data from
+const [x, y, z] = arr;
+console.log(x, y, z);
+
+// the multiple commas in the constant allows me to retrieve  the specific item from the array (I can skip items)
+let [main, , secondary] = restaurant.categories;
+console.log(main, secondary);
+
+// switches variables
+[main, secondary] = [secondary, main];
+console.log(main, secondary);
+
+//   openingHours: {
+//     thu: {
+//       open: 12,
+//       close: 22,
+//     },
+//     fri: {
+//       open: 11,
+//       close: 23,
+//     },
+//     sat: {
+//       open: 0, // Open 24 hours
+//       close: 24,
+//     },
+//   },
+//
