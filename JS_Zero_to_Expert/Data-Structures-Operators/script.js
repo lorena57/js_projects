55900503af9e0727b7b2bf9fef525733de71927e
Lorena C <lorena@eunknown.org>
2021-01-28 21:13:55 -0800

    destructuring nested arrays

--- JS_Zero_to_Expert/Data-Structures-Operators/script.js
+++ JS_Zero_to_Expert/Data-Structures-Operators/script.js
@@ -12,12 +12,12 @@ const restaurant = {
   },
 };
 
-console.log(restaurant.order(2, 0));
+// console.log(restaurant.order(2, 0));
 // To destructure do the following:
 
 // starter is the 2nd item from the array and 0 is the 1st item from the array;
 const [starter, mainMeal] = restaurant.order(2, 0);
-console.log(starter, mainMeal);
+// console.log(starter, mainMeal);
 
 const arr = [2, 3, 4];
 const a = arr[0];
@@ -33,11 +33,11 @@ const [x, y, z] = arr;
 
 // main is Italian and secondary is Vegetarian (the , skips an object)
 let [main, , secondary] = restaurant.categories;
-console.log(main, secondary);
+// console.log(main, secondary);
 
 // switches variables
 [main, secondary] = [secondary, main];
-console.log(main, secondary);
+// console.log(main, secondary);
 
 //   openingHours: {
 //     thu: {
@@ -54,3 +54,16 @@ console.log(main, secondary);
 //     },
 //   },
 //
+
+const nested = [2, 4, [5, 6]];
+
+// i is the 2 and j is the 5 & 6 (, skips # 4)
+// currently i is not in its own array and j is its own array with 5 & 6
+// const [i, , j] = nested;
+
+// to destructure again
+const [i, , [j, k]] = nested;
+
+// console.log(i, j);
+// Now 2 5 and 6 are there own objects and not in an array
+console.log(i, j, k);
