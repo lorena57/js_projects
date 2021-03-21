5bc63415ddc6bf9bf9414e85ec86aa243e276069
Lorena C <lorena@eunknown.org>
2021-03-20 18:53:04 -0700

    update git

--- JS_Zero_to_Expert/Guess My Number/script.js
+++ JS_Zero_to_Expert/Guess My Number/script.js
@@ -24,12 +24,22 @@ document.querySelector('.check').addEventListener('click', () => {
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number!';
   } else if (guess > secretNumber) {
-    document.querySelector('.message').textContent = '📈 Too High';
-    score--;
-    document.querySelector('.score').textContent = score;
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Too High';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
   } else if (guess < secretNumber) {
-    document.querySelector('.message').textContent = '📉 Too Low';
-    score--;
-    document.querySelector('.score').textContent = score;
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Too low';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
