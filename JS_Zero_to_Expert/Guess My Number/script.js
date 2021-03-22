099bd179f82b0ef2ba685e8624821df57317d74b
Lorena C <lorena@eunknown.org>
2021-03-22 13:00:59 -0700

    set high scores

--- JS_Zero_to_Expert/Guess My Number/script.js
+++ JS_Zero_to_Expert/Guess My Number/script.js
@@ -1,8 +1,8 @@
 'use strict';
 
 let score = 20;
-
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let highScore = 0;
 
 document.querySelector('.check').addEventListener('click', () => {
   const guess = Number(document.querySelector('.guess').value);
@@ -17,6 +17,11 @@ document.querySelector('.check').addEventListener('click', () => {
     document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
 
     // When guess is too high
   } else if (guess > secretNumber) {
