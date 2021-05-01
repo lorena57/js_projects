1fadeb77bdbb0aa5a01e05e6ac2434143a526088
Lorena C <lorena@eunknown.org>
2021-04-30 23:16:47 -0700

    fix backtick bug

--- JS_Zero_to_Expert/Section 16/script.js
+++ JS_Zero_to_Expert/Section 16/script.js
@@ -108,7 +108,14 @@ const renderCountry = function (data, className = '') {
 const getCountryData = function (country) {
   fetch(`https://restcountries.eu/rest/v2/name/${country}`)
     .then((response) => response.json())
-    .then((data) => renderCountry(data[0]));
+    .then((data) => {
+      renderCountry(data[0]);
+      const neighbor = data[0].borders[0];
+      if (!neighbor) return;
+      return fetch(`https://restcountries.eu/rest/v2/alpha/${neighbor}`)
+        .then((response) => response.json())
+        .then((data) => renderCountry(data, 'neighbor'));
+    });
 };
 
-getCountryData('mexico');
+getCountryData('portugal');
