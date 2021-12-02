7d0bced634172e10e192e9fe11868875d590dc50
Lorena C <lorena@eunknown.org>
2021-12-02 06:24:44 -0800

    redo again

--- 50_projects_in_50_days/Expanding Cards/script.js
+++ 50_projects_in_50_days/Expanding Cards/script.js
@@ -1,16 +1,31 @@
-// querySelectorAll creates a NodeList of the divs and saves it to the const
-const panels = document.querySelectorAll('.panel')
+// // querySelectorAll creates a NodeList of the divs and saves it to the const
+// const panels = document.querySelectorAll('.panel')
 
-//panels loops through each panel console.log(panel)
-panels.forEach(panel => {
-    panel.addEventListener('click', () => {
-        removeActiveClasses()
-        panel.classList.add('active')
-    })
-})
+// //panels loops through each panel console.log(panel)
+// panels.forEach(panel => {
+//     panel.addEventListener('click', () => {
+//         removeActiveClasses()
+//         panel.classList.add('active')
+//     })
+// })
+
+// function removeActiveClasses() {
+//     panels.forEach(panel => {
+//         panel.classList.remove('active')
+//     })
+// }
+
+const panels = document.querySelectorAll('.panel');
+
+panels.forEach((panel) => {
+  panel.addEventListener('click', () => {
+    removeActiveClasses();
+    panel.classList.add('active');
+  });
+});
 
 function removeActiveClasses() {
-    panels.forEach(panel => {
-        panel.classList.remove('active')
-    })
+  panels.forEach((panel) => {
+    panel.classList.remove('active');
+  });
 }
