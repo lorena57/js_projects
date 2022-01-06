231d585d33c612dd10fd4f762b15d0a670754553
Lorena C <lorena@eunknown.org>
2022-01-05 20:47:23 -0800

    process assignment operators

--- JS_Zero_to_Expert/01_Fundamentals_Pt_1/script.js
+++ JS_Zero_to_Expert/01_Fundamentals_Pt_1/script.js
@@ -1,23 +1,37 @@
-let firstName = 'Lorena';
-console.log(firstName);
+// let firstName = 'Lorena';
+// console.log(firstName);
 
-let javaScriptIsHard = true;
-console.log(javaScriptIsHard);
+// let javaScriptIsHard = true;
+// console.log(javaScriptIsHard);
 
-javaScriptIsHard = "I'm here to learn!";
-console.log(javaScriptIsHard);
+// javaScriptIsHard = "I'm here to learn!";
+// console.log(javaScriptIsHard);
 
-let year;
-console.log(year);
+// let year;
+// console.log(year);
 
-year = 2022;
-console.log(year);
+// year = 2022;
+// console.log(year);
 
-let age = 18;
+// let age = 18;
 //let variable are mutable
-age = 19;
-console.log(age);
+// age = 19;
+// console.log(age);
 
-const birthYear = 1957;
+// const birthYear = 1957;
 //const variable are immutable
 // birthYear = 1975;
+
+// const presentYear = 2037;
+
+// const ageLenny = presentYear - 1980;
+// console.log(ageLenny);
+
+// const lastName = 'Smith';
+// const firstName = 'Lenny';
+
+// console.log(lastName + ' ' + firstName);
+
+// let x = 10 + 5;
+// x += 10; //+= is adding what is to the right of the operator, 25
+// console.log(x);
