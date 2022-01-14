70f8bdf3c3a2c3c74257a1e0e6d1c52c1c00a562
Lorena C <lorena@eunknown.org>
2022-01-13 22:02:01 -0800

    create function declarations and function expressions

--- JS_Zero_to_Expert/02_Fundamentals_Pt_2/script.js
+++ JS_Zero_to_Expert/02_Fundamentals_Pt_2/script.js
@@ -12,3 +12,20 @@ console.log(fruitProcessor(5, 7));
 
 const appleOrangeJuice = fruitProcessor(3, 5);
 console.log(appleOrangeJuice);
+
+//Function declaration
+function calcAge1(birthYear) {
+  return 2057 - birthYear;
+}
+
+let age1 = calcAge1(2022);
+console.log(age1);
+
+//Function expression
+let tester = function (birthYear) {
+  return 2057 - birthYear;
+};
+
+let age2 = tester(2022);
+
+console.log(age2);
