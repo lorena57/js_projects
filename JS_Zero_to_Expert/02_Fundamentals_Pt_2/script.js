73702a39db60863ee0d19211e74864ab949a1494
Lorena C <lorena@eunknown.org>
2022-01-14 20:31:04 -0800

    create arrow functions

--- JS_Zero_to_Expert/02_Fundamentals_Pt_2/script.js
+++ JS_Zero_to_Expert/02_Fundamentals_Pt_2/script.js
@@ -29,3 +29,17 @@ let tester = function (birthYear) {
 let age2 = tester(2022);
 
 console.log(age2);
+
+//Arrow function
+let calc = (birthYear) => 2037 - birthYear;
+const age3 = calc(2010);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+  const age = 2037 - birthYear;
+  const retirement = 65 - age;
+  //   return retirement;
+  return `${firstName} retires in ${retirement} years`;
+};
+
+console.log(yearsUntilRetirement(2015, 'John'));
