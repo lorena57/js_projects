37e192358d4f9e5deb4c67c21683e3d74cdb611c
Lorena C <lorena@eunknown.org>
2022-02-02 19:36:15 -0800

    create a function in a function

--- JS_Zero_to_Expert/Section8/src/script.js
+++ JS_Zero_to_Expert/Section8/src/script.js
@@ -2,7 +2,12 @@
 
 function calcAge(birthYear) {
   const age = 2037 - birthYear;
-  console.log(firstName);
+
+  function printAge() {
+    const output = `${firstName}, you are ${age}, born in ${birthYear}`;
+    console.log(output);
+  }
+  printAge();
   return age;
 }
 
