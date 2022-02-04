aa030243242f2d32e1f6678c2e94711eb78e8805
Lorena C <lorena@eunknown.org>
2022-02-03 20:29:30 -0800

    create function and a var variable

--- JS_Zero_to_Expert/Section8/src/script.js
+++ JS_Zero_to_Expert/Section8/src/script.js
@@ -34,18 +34,28 @@
 // let job = 'Disneyland';
 // const year = 2000;
 
-console.log(addDelc(2, 3));
+// console.log(addDelc(2, 3));
 
-console.log(addExpr(2, 3));
+// console.log(addExpr(2, 3));
 
-console.log(addArrow(2, 3));
+// console.log(addArrow(2, 3));
 
-function addDelc(a, b) {
-  return a + b;
+// function addDelc(a, b) {
+//   return a + b;
+// }
+
+// const addExpr = function (a, b) {
+//   return a + b;
+// };
+
+// const addArrow = (a, b) => a + b;
+
+console.log(numProducts);
+
+if (!numProducts) deleteShoppingCart();
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+  console.log('All products deleted');
 }
-
-const addExpr = function (a, b) {
-  return a + b;
-};
-
-const addArrow = (a, b) => a + b;
