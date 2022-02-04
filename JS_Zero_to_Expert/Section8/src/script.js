9d59781a960ddbb86a502d9c696619a715dc8e54
Lorena C <lorena@eunknown.org>
2022-02-03 19:58:15 -0800

    comment out function to practice hoisting

--- JS_Zero_to_Expert/Section8/src/script.js
+++ JS_Zero_to_Expert/Section8/src/script.js
@@ -1,26 +1,34 @@
 'use strict';
 
-function calcAge(birthYear) {
-  const age = 2037 - birthYear;
+// function calcAge(birthYear) {
+//   const age = 2037 - birthYear;
 
-  function printAge() {
-    const output = `${firstName}, you are ${age}, born in ${birthYear}`;
-    console.log(output);
+//   function printAge() {
+//     const output = `${firstName}, you are ${age}, born in ${birthYear}`;
+//     console.log(output);
 
-    if (birthYear >= 1981 && birthYear <= 1996) {
-      var millenial = true;
-      const firstName = 'Mickey';
-      const str = `Oh, and you're a millenial, ${firstName}`;
-      console.log(str);
+//     if (birthYear >= 1981 && birthYear <= 1996) {
+//       var millenial = true;
+//       const firstName = 'Mickey';
+//       const str = `Oh, and you're a millenial, ${firstName}`;
+//       console.log(str);
 
-      function add(a, b) {
-        return a + b;
-      }
-    }
-  }
-  printAge();
-  return age;
-}
+//       function add(a, b) {
+//         return a + b;
+//       }
+//     }
+//   }
+//   printAge();
+//   return age;
+// }
 
-const firstName = 'Minnie';
-calcAge(1991);
+// const firstName = 'Minnie';
+// calcAge(1991);
+
+console.log(me);
+console.log(job);
+console.log(year);
+
+var me = 'Minnie';
+let job = 'Disneyland';
+const year = 2000;
