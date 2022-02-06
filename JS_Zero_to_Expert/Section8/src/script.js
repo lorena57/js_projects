9ed832d1efe09653818064bed5e11b4d065805c7
Lorena C <lorena@eunknown.org>
2022-02-05 21:03:41 -0800

    arrow function getting this from parrent

--- JS_Zero_to_Expert/Section8/src/script.js
+++ JS_Zero_to_Expert/Section8/src/script.js
@@ -116,11 +116,18 @@ const minnie = {
   calcAge: function () {
     console.log(2037 - this.year);
 
-    const self = this;
-    const isMillenial = function () {
-      console.log(self);
-      console.log(self.year >= 1981 && self.year <= 1996);
+    //Solution 1
+    // const self = this;
+    // const isMillenial = function () {
+    //   console.log(self);
+    //   console.log(self.year >= 1981 && self.year <= 1996);
+    // };
+    //Solution 2
+    const isMillenial = () => {
+      console.log(this);
+      console.log(this.year >= 1981 && this.year <= 1996);
     };
+
     isMillenial();
   },
 };
