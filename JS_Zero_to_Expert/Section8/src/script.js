5f38f284aa5e43cbd4a71599839dc8cac36e917f
Lorena C <lorena@eunknown.org>
2022-02-06 21:12:41 -0800

    commit out code

--- JS_Zero_to_Expert/Section8/src/script.js
+++ JS_Zero_to_Expert/Section8/src/script.js
@@ -110,26 +110,26 @@
 
 // minnie.calcAge();
 
-const minnie = {
-  firstName: 'Minnie',
-  year: 1991,
-  calcAge: function () {
-    console.log(2037 - this.year);
+// const minnie = {
+//   firstName: 'Minnie',
+//   year: 1991,
+//   calcAge: function () {
+//     console.log(2037 - this.year);
 
-    //Solution 1
-    // const self = this;
-    // const isMillenial = function () {
-    //   console.log(self);
-    //   console.log(self.year >= 1981 && self.year <= 1996);
-    // };
-    //Solution 2
-    const isMillenial = () => {
-      console.log(this);
-      console.log(this.year >= 1981 && this.year <= 1996);
-    };
+//Solution 1
+// const self = this;
+// const isMillenial = function () {
+//   console.log(self);
+//   console.log(self.year >= 1981 && self.year <= 1996);
+// };
+//Solution 2
+//     const isMillenial = () => {
+//       console.log(this);
+//       console.log(this.year >= 1981 && this.year <= 1996);
+//     };
 
-    isMillenial();
-  },
-};
+//     isMillenial();
+//   },
+// };
 
-minnie.calcAge();
+// minnie.calcAge();
