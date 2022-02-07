86e534017facaa3626315dd26487f2126c605162
Lorena C <lorena@eunknown.org>
2022-02-07 07:37:23 -0800

    create function

--- JS_Zero_to_Expert/section9/script.js
+++ JS_Zero_to_Expert/section9/script.js
@@ -6,16 +6,26 @@ const restaurant = {
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
 };
 
 let [main, , secondary] = restaurant.categories;
 
 console.log(main, secondary);
 
+//Switching arrays Example 1
 // const temp = main;
 // main = secondary;
 // secondary = temp;
 // console.log(main, secondary);
 
+//Switching arrays example 2
 [main, secondary] = [secondary, main];
 console.log(main, secondary);
+
+//Receive 2 return values from a function
+const [starter, mainCourse] = restaurant.order(2, 0);
+console.log(starter, mainCourse);
