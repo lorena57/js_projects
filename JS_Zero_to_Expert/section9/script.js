25422088c68ffd3ebc0bb969efa824fa207e1062
Lorena C <lorena@eunknown.org>
2022-02-07 07:25:37 -0800

    destructuring an array

--- JS_Zero_to_Expert/section9/script.js
+++ JS_Zero_to_Expert/section9/script.js
@@ -1,9 +1,21 @@
 'use strict';
 
-const resturant = {
+const restaurant = {
   name: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze, Italy',
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 };
+
+let [main, , secondary] = restaurant.categories;
+
+console.log(main, secondary);
+
+// const temp = main;
+// main = secondary;
+// secondary = temp;
+// console.log(main, secondary);
+
+[main, secondary] = [secondary, main];
+console.log(main, secondary);
