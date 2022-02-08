e6a64d399f06dd4f85d8e6da61fe6f79b59b6e78
Lorena C <lorena@eunknown.org>
2022-02-07 19:14:14 -0800

    destructuring an object

--- JS_Zero_to_Expert/section9/script.js
+++ JS_Zero_to_Expert/section9/script.js
@@ -1,20 +1,20 @@
 'use strict';
 
-const restaurant = {
-  name: 'Classico Italiano',
-  location: 'Via Angelo Tavanti 23, Firenze, Italy',
-  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+// const restaurant = {
+//   name: 'Classico Italiano',
+//   location: 'Via Angelo Tavanti 23, Firenze, Italy',
+//   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+//   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+//   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-  order: function (starterIndex, mainIndex) {
-    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
-  },
-};
+//   order: function (starterIndex, mainIndex) {
+//     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+//   },
+// };
 
-let [main, , secondary] = restaurant.categories;
+// let [main, , secondary] = restaurant.categories;
 
-console.log(main, secondary);
+// console.log(main, secondary);
 
 //Switching arrays Example 1
 // const temp = main;
@@ -23,21 +23,59 @@ console.log(main, secondary);
 // console.log(main, secondary);
 
 //Switching arrays example 2
-[main, secondary] = [secondary, main];
-console.log(main, secondary);
+// [main, secondary] = [secondary, main];
+// console.log(main, secondary);
 
 //Receive 2 return values from a function
-const [starter, mainCourse] = restaurant.order(2, 0);
-console.log(starter, mainCourse);
+// const [starter, mainCourse] = restaurant.order(2, 0);
+// console.log(starter, mainCourse);
 
-const nested = [1, 2, [4, 5]];
+// const nested = [1, 2, [4, 5]];
 
-const [i, , j] = nested;
+// const [i, , j] = nested;
 
-console.log(i, j);
+// console.log(i, j);
 
-const newlyNested = ['One', 'Two', 'Three', ['Four', 'Five']];
+// const newlyNested = ['One', 'Two', 'Three', ['Four', 'Five']];
 
-const [a, b, , [c, d]] = newlyNested;
+// const [a, b, , [c, d]] = newlyNested;
 
-console.log(a, b, c, d);
+// console.log(a, b, c, d);
+
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0,
+      close: 14,
+    },
+  },
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+};
+
+const { name, openingHours, categories } = restaurant;
+
+// console.log(name, openingHours, categories);
+
+const {
+  name: restaurantName,
+  openingHours: hours,
+  categories: tags,
+} = restaurant;
+
+console.log(restaurantName, hours, tags);
