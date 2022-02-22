d946690402a8b1e6e2ded6fd7284788e430006dd
Lorena C <lorena@eunknown.org>
2022-02-21 20:51:32 -0800

    loop through object

--- JS_Zero_to_Expert/section9/script.js
+++ JS_Zero_to_Expert/section9/script.js
@@ -290,6 +290,16 @@ const openingHours = {
   },
 };
 
-for (const day of Object.keys(openingHours)) {
-  console.log(day);
+const properties = Object.keys(openingHours);
+console.log(properties);
+
+let openStr = `We are open on ${properties.length} days: `;
+
+// for (const day of Object.keys(openingHours)) {
+//   console.log(day);
+// }
+
+for (const day of properties) {
+  openStr += `${day},`;
 }
+console.log(openStr);
