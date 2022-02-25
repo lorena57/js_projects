b657733994314569d4180db43ff30f2f64b0ca9e
Lorena C <lorena@eunknown.org>
2022-02-24 21:06:30 -0800

    add notes

--- JS_Zero_to_Expert/section9/script.js
+++ JS_Zero_to_Expert/section9/script.js
@@ -428,10 +428,14 @@ for (const [key, value] of question) {
   if (typeof key === 'number') console.log(`Answer ${key}: ${value}`);
 }
 
-const answer = Number(prompt('Your answer'));
+// const answer = Number(prompt('Your answer'));
+const answer = 3;
 console.log(answer);
 
 console.log(question.get(question.get('correct') === answer));
 
 //Convert map to array
 console.log([...question]);
+console.log(question.entries());
+console.log(question.keys());
+console.log(question.values());
