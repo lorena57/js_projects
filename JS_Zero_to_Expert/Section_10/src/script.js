0a58d86e604c8736a103dbcbb251249b98ef195a
Lorena C <lorena@eunknown.org>
2022-03-03 21:35:19 -0800

    add an object to the funciton

--- JS_Zero_to_Expert/Section_10/src/script.js
+++ JS_Zero_to_Expert/Section_10/src/script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const bookings = [];
+
+const createBooking = function (flightNum, numPassengers = 1, price = 199) {
+  //   numPassengers = numPassengers || 1;
+  //   price = price || 199;
+
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  console.log(bookings);
+  bookings.push(booking);
+};
+
+createBooking('MC123');
