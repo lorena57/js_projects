267960785e85ff82407808c25a9e3f59ffdb30cf
Lorena C <lorena@eunknown.org>
2022-03-04 23:07:10 -0800

    create function

--- JS_Zero_to_Expert/Section_10/src/script.js
+++ JS_Zero_to_Expert/Section_10/src/script.js
@@ -19,3 +19,24 @@
 // createBooking('MC123');
 // createBooking('123456');
 // createBooking('ABCDEF');
+
+const flight = 'LH234';
+
+const whoAmI = {
+  name: 'Minnie Mouse',
+  passport: 461484521,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LA123';
+  passenger.name = 'Mrs.' + passenger.name;
+
+  if (passenger.passport === 461484521) {
+  } else {
+    alert('wrong passport!');
+  }
+};
+
+checkIn(flight, whoAmI);
+console.log(flight);
+console.log(whoAmI);
