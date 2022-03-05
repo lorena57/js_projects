7d82aa10ff7fc40a288a11c77f007747b763725e
Lorena C <lorena@eunknown.org>
2022-03-04 22:48:44 -0800

    comment out code

--- JS_Zero_to_Expert/Section_10/src/script.js
+++ JS_Zero_to_Expert/Section_10/src/script.js
@@ -1,19 +1,21 @@
 'use strict';
 
-const bookings = [];
+// const bookings = [];
 
-const createBooking = function (flightNum, numPassengers = 1, price = 199) {
-  //   numPassengers = numPassengers || 1;
-  //   price = price || 199;
+// const createBooking = function (flightNum, numPassengers = 1, price = 199) {
+//   numPassengers = numPassengers || 1;
+//   price = price || 199;
 
-  const booking = {
-    flightNum,
-    numPassengers,
-    price,
-  };
-  console.log(booking);
-  console.log(bookings);
-  bookings.push(booking);
-};
+//   const booking = {
+//     flightNum,
+//     numPassengers,
+//     price,
+//   };
+//   console.log(booking);
+//   console.log(bookings);
+//   bookings.push(booking);
+// };
 
-createBooking('MC123');
+// createBooking('MC123');
+// createBooking('123456');
+// createBooking('ABCDEF');
