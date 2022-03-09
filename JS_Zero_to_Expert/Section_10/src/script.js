ebc5b282367025aa1d2d7c0505227294eab03710
Lorena C <lorena@eunknown.org>
2022-03-08 21:22:02 -0800

    create callback function

--- JS_Zero_to_Expert/Section_10/src/script.js
+++ JS_Zero_to_Expert/Section_10/src/script.js
@@ -60,3 +60,12 @@ const upperFirstWord = function (str) {
   const [first, ...others] = str.split(' ');
   return [first.toUpperCase(), ...others].join(' ');
 };
+
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer("I'm the best!", upperFirstWord);
