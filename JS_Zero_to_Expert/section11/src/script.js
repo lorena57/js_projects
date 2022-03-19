53b23e1c0501a9e806c8059bab7d2f71f8c8033a
Lorena C <lorena@eunknown.org>
2022-03-18 21:10:08 -0700

    comment out code

--- JS_Zero_to_Expert/section11/src/script.js
+++ JS_Zero_to_Expert/section11/src/script.js
@@ -2,33 +2,32 @@
 
 //SLICE
 
-let arr = ['a', 'b', 'c', 'd', 'e'];
+// let arr = ['a', 'b', 'c', 'd', 'e'];
 
-console.log(arr.slice(2));
+// console.log(arr.slice(2));
 
 //The end parameter is not included
 // 2 and 4 is really just 2 and 3.
-console.log(arr.slice(2, 4));
+// console.log(arr.slice(2, 4));
 
-console.log(arr.slice(-2));
+// console.log(arr.slice(-2));
 
-console.log(arr.slice(1, -2));
+// console.log(arr.slice(1, -2));
 
 //SPLICE
 //SPLICE mutates the array
 
-console.log(arr.splice(2));
+// console.log(arr.splice(2));
 //-1 removes the last item from the array
-arr.splice(-1);
+// arr.splice(-1);
 
 //Reverse
 //Reverse mutates the array
-let arr2 = ['j', 'i', 'h', 'g', 'f'];
+// let arr2 = ['j', 'i', 'h', 'g', 'f'];
 
-console.log(arr2.reverse());
+// console.log(arr2.reverse());
 
 //concat
-const letters = arr.concat(arr2);
+// const letters = arr.concat(arr2);
 //or you can
-
-console.log([...arr, ...arr2]);
+// console.log([...arr, ...arr2]);
