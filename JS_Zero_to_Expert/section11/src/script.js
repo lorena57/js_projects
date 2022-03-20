e615d5010551b9a366d94febc00943782c03707a
Lorena C <lorena@eunknown.org>
2022-03-19 20:30:56 -0700

    write a for forEach loop

--- JS_Zero_to_Expert/section11/src/script.js
+++ JS_Zero_to_Expert/section11/src/script.js
@@ -33,12 +33,22 @@
 // console.log([...arr, ...arr2]);
 
 //At method
-const arr = [23, 11, 64];
-console.log(arr.at(0));
+// const arr = [23, 11, 64];
+// console.log(arr.at(0));
 
-console.log(arr[arr.length - 1]);
+// console.log(arr[arr.length - 1]);
 //Copy of the original array
-console.log(arr.slice(-1)[0]);
-console.log(arr.at(-1));
+// console.log(arr.slice(-1)[0]);
+// console.log(arr.at(-1));
 
-console.log('Minnie'.at(0));
+// console.log('Minnie'.at(0));
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+for (const movement of movements) {
+  if (movement > 0) {
+    console.log(`You deposited ${movement}`);
+  } else {
+    console.log(`You withdrew ${Math.abs(movement)}`);
+  }
+}
